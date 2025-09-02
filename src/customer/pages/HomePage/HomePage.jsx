163252a5e0c5db4ac8b795d6a9625cf938eaff58
SHIVAM <145546564+MAJOR-SHIVAM@users.noreply.github.com>
2025-09-02 15:19:32 +0530

    initial commit

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCrosel from '../../components/HomeCarosel/MainCrosel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCrosel/>
+        <div>
+            Other Sections
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
